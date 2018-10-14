067a7e197ea324be888cde0f9d964a6d39990f84
Matt Mawhinney <mattmawhinney@live.com>
2018-10-14 14:27:23 -0400

    Added Parcel as bundle tool

--- src/js/file1.js
+++ src/js/file1.js
@@ -1,9 +1,9 @@
-import '../assets/img/wifi.png';
-import '../css/style.css';
-import { LiveServer } from './file2';
+import "../assets/img/wifi.png";
+import "../css/style.css";
+import { LiveServer } from "./file2";
 
-console.log('ES6', () => {
-    console.log('Thank you for using Static-Live-Server!');
+console.log("ES6", () => {
+  console.log("Thank you for using Static-Live-Server!");
 });
 
 LiveServer.consoleServer();
